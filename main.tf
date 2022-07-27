8a1271b9ea53c6111d642770e342623f4ec566c6
Shahbaz Alam <shahbaz.alam@synectiks.com>
2022-07-27 13:40:15 +0530

    update main

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudformation_stack" "dms-stack" {
+  name = "dms-cf-stack-2"
+
+  template_url = "https://dmsdeploy-code.s3.amazonaws.com/dms_launch.json"
+    
+  parameters = {
+    AdminEmail       = "shahbaz.alam@synectiks.com"
+    AppEnvironment   = "stage"
+    EnablePublicUrls = "true"
+  }
+  capabilities = ["CAPABILITY_IAM", "CAPABILITY_AUTO_EXPAND", "CAPABILITY_NAMED_IAM"]
+}
